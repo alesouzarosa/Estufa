044dbec29dbe8013b9ad399d5bc510f2b7543a1e
Alexandre de Souza Rosa <alesouzarosa@gmail.com>
2021-05-23 16:00:18 -0300

    primeiro commit

---
+++ estufa.X/main.c
@@ -0,0 +1,21 @@
+/*
+ * File:   main.c
+ * Author: Alexandre de Souza Rosa
+ * Email: alesouzarosa@gmail.com
+ *
+ * Projeto: Estufa
+ * 
+ * 
+ * Created on May 14, 2021, 2:13 PM
+ */
+
+
+
+
+
+#include <xc.h>
+
+void main(void)
+{
+   return;
+}
